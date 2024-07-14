e3feb3493d9a7d396a9673c3ab14024c9e967caf
Deepak Choure <dchoure2306@gmail.com>
2024-07-14 16:28:43 +0530

    day2 done

---
+++ day2/script.js
@@ -0,0 +1,67 @@
+//activity 1
+//task 1 wap to add 2 number and log the result
+let num1 = 20;
+let num2 = 30;
+let result = num1 + num2;
+console.log(result);
+//task 2 wap to substract 2 number and log the result
+let a = 30;
+let b = 20;
+let c = a-b;
+console.log(c);
+//task 3 wap to multiply the 2 num and log the result
+let x = 12;
+let y = 10;
+let z = x*y;
+console.log(z);
+//task 4 divide 
+let m = 45;
+let n = 15;
+let o = m/n;
+console.log(o);
+//task 5 find remainder when one number divided by other
+let p = 18
+let q = 5;
+let rem = p%q;
+console.log(rem);
+
+//activity 2 Assignment oprator += and -= 
+//task 6
+let num =5;
+num+=3;
+console.log(num);
+//task 7
+num-=3;
+console.log(num);
+
+//Activity 3
+//task 8
+console.log(5>3);
+console.log(3>5);
+console.log(3<8);
+console.log(8<3);
+//task 9
+console.log(8>=7);
+console.log(8>=9);
+console.log(8>=8);
+console.log(5<=6);
+console.log(5<=4);
+console.log(5<=5);
+//task 10
+console.log(5==5);
+console.log("5"==5);
+console.log("5"=== 5);
+//task 11
+console.log(true && true);
+console.log(false && true);
+console.log(false && false);
+//task 12
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+//task 13
+console.log(!true);
+console.log(!false);
+//task 14 
+let number = -6
+console.log(number>0 ? "positive":"negative");
